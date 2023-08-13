450bebdb4be340e99c478d1ee2d8b1176cc7b9c6
HarryKing87 <babishh96@hotmail.com>
2023-08-13 12:30:28 +0300

    Show name on the NFT collection

--- opend/src/opend_assets/src/components/App.jsx
+++ opend/src/opend_assets/src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "./Header";
+import Item from "./Item";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
 
 function App() {
+  const NFTId = "ryjl3-tyaaa-aaaaa-aaaba-cai";
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      <Item id={NFTId}/>
+      {/*<img className="bottom-space" src={homeImage} />*/}
       <Footer />
     </div>
   );
